fb801d906708f7af921be479701643ff9b11d679
nemanjaASE <nightwalker44ASE@gmail.com>
2022-05-21 19:07:01 +0200

    New commit

---
+++ parfemi/Models/Parfem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parfemi.Models
+{
+    public class Parfem
+    {
+        public string Id { get; set; }
+        public string Naziv { get; set; }
+        public MirisnaNota Nota { get; set; }
+        public int Cena { get; set; }
+        public bool Akcija { get; set; }
+    }
+}
